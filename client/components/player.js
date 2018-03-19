bc260235ba65562ca416cbc7a3bb767fa27e1db5
Joshua Sohn <joshuadsohn@gmail.com>
2018-03-18 23:23:59 -0400

    replaces HTML audio tag with react-modular-audio-player, adds styling for audioplayer component

--- client/components/player.js
+++ client/components/player.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Audio } from './index'
+import React from "react";
+import { Audio } from "./index";
+import AudioPlayer from "react-modular-audio-player";
 
-export const Player = (props) => {
-
+export const Player = props => {
   return (
     <div className="audio">
-      <div className="audio-title" >
+      <div className="audio-title">
         <h2>Examples </h2>
       </div>
       <div className="audio-examples">
-        <div className="audio-examples-title">
-          Audiobook
-        </div>
-        <Audio mp3="/audio/Pride and Prejudice.mp3" />
+        <div className="audio-examples-title">Audiobook</div>
+        <AudioPlayer
+          audioFiles={[{ src: "/audio/Pride and Prejudice.mp3" }]}
+          playerWidth={"90%"}
+          hideForward={true}
+          hideLoop={true}
+          hideRewind={true}
+          hideName={true}
+          hideSeeking={true}
+          iconSize={"1.7rem"}
+          fontSize={"1.2rem"}
+        />
       </div>
       <div className="audio-examples">
-        <div className="audio-examples-title">
-          Educational
-        </div>
-        <Audio mp3="/audio/Test Cut.mp3" />
+        <div className="audio-examples-title">Educational</div>
+        <AudioPlayer
+          audioFiles={[{ src: "/audio/Test Cut.mp3" }]}
+          playerWidth={"90%"}
+          hideForward={true}
+          hideLoop={true}
+          hideRewind={true}
+          hideName={true}
+          hideSeeking={true}
+          iconSize={"1.7rem"}
+          fontSize={"1.2rem"}
+        />
       </div>
       <div className="audio-examples">
-        <div className="audio-examples-title">
-          Podcast
-        </div>
-        <Audio mp3="/audio/Weekly Weather Minute.mp3" />
+        <div className="audio-examples-title">Podcast</div>
+        <AudioPlayer
+          audioFiles={[{ src: "/audio/Weekly Weather Minute.mp3" }]}
+          playerWidth={"90%"}
+          hideForward={true}
+          hideLoop={true}
+          hideRewind={true}
+          hideName={true}
+          hideSeeking={true}
+          iconSize={"1.7rem"}
+          fontSize={"1.2rem"}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
